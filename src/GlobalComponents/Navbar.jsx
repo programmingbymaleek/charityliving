c03aef1c888b2c5aa73564c380fedfb4f34b16ea
mary <adebimpecole@gmail.com>
2023-11-21 00:00:24 +0100

    forgot one

--- src/GlobalComponents/Navbar.jsx
+++ src/GlobalComponents/Navbar.jsx
@@ -1,57 +1,86 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useRef, useEffect, useContext } from 'react'
 import { Link } from "react-router-dom";
+
+import { Context } from '../Utilities/Context'
 
 import styled from 'styled-components'
 import menu from "../assets/close.svg"
 
-const Navbar = ({ left, leftscroll, right, rightscroll, signincolor, joincolor, joincolorscroll, joinborderscroll, signincolorscroll, signinscroll, siginborder, siginborderscroll, joinborder, joinbackground, the_scroll }) => {
+const Navbar = () => {
+    let { navStyleObject} = useContext(Context)
 
     const [scroll, setScroll] = useState(false);
 
+    const [data, setData] = useState({});
+
+
+    const [loading, setLoading] = useState(true);
+
     const handleScroll = () => {
-        if (window.scrollY > the_scroll) {
+        if (window.scrollY > navStyleObject.the_scroll) {
             setScroll(true);
         } else {
             setScroll(false);
         }
     };
-    window.onscroll = handleScroll;
 
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        setLoading(true)
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    
+    
+    useEffect(() => {
+        if( Object.keys(navStyleObject).length === 0 ){
+            setLoading(true)
+        }
+        else{
+            console.log(navStyleObject)
+            setData(navStyleObject)
+            setLoading(false)
+        }
+        console.log(loading)
+    }, [navStyleObject])
 
     return (
-        <Navdiv scroll={scroll}>
-            <Leftnav left={left} scroll={scroll} leftscroll={leftscroll}>
-                <Ul>
-                    <li>logo</li>
-                </Ul>
-                <Ul>
-                    <li>
-                        <SelectField id="option" name="option">
-                            <option value="1">Take Action</option>
-                            <option value="2">Option 2</option>
-                        </SelectField>
-                    </li>
-                    <li>
-                        <SelectField id="option1" name="option">
-                            <option value="1">About Us</option>
-                            <option value="2">Option 2</option>
-                        </SelectField>
-                    </li>
-                    <li>Why water?</li>
-                </Ul>
-            </Leftnav>
-            <Rightnav right={right} scroll={scroll} rightscroll={rightscroll}>
-                <Ul>
-                    <Link to='/givepage' className='pick_user' ><Button>Give</Button></Link>
-                    <ButtonJoin joinbackground={joinbackground} joincolor={joincolor} scroll={scroll} joinborder={joinborder} joinborderscroll={joinborderscroll} joincolorscroll={joincolorscroll} >Join the springs</ButtonJoin>
-                </Ul>
-                <Ul>
-                    <Link to='/signin' className='pick_user' ><SignInButton scroll={scroll} siginborder={siginborder} siginborderscroll={siginborderscroll} signincolorscroll={signincolorscroll} signincolor={signincolor}>Sign in</SignInButton></Link>
-                </Ul>
-            </Rightnav>
-            <Hamburger src={menu} alt='menu' />
+        loading === false ? (
+            <Navdiv scrolled={scroll}>
+                <Leftnav left={data.left} scrolled={scroll} leftscroll={data.leftscroll}>
+                    <Ul>
+                        <li>logo</li>
+                    </Ul>
+                    <Ul>
+                        <li>
+                            <SelectField id="option" name="option">
+                                <option value="1">Take Action</option>
+                                <option value="2">Option 2</option>
+                            </SelectField>
+                        </li>
+                        <li>
+                            <SelectField id="option1" name="option">
+                                <option value="1">About Us</option>
+                                <option value="2">Option 2</option>
+                            </SelectField>
+                        </li>
+                        <li>Why water?</li>
+                    </Ul>
+                </Leftnav>
+                <Rightnav right={data.right} scrolled={scroll} rightscroll={data.rightscroll}>
+                    <Ul>
+                        <Link to='/givepage' className='pick_user' ><Button>Give</Button></Link>
+                        <ButtonJoin joinbackground={data.joinbackground} joincolor={data.joincolor} scrolled={scroll} joinborder={data.joinborder} joinborderscroll={data.joinborderscroll} joincolorscroll={data.joincolorscroll} >Join the springs</ButtonJoin>
+                    </Ul>
+                    <Ul>
+                        <Link to='/signin' className='pick_user' ><SignInButton scrolled={scroll} siginborder={data.siginborder} siginborderscroll={data.siginborderscroll} signincolorscroll={data.signincolorscroll} signincolor={data.signincolor}>Sign in</SignInButton></Link>
+                    </Ul>
+                </Rightnav>
+                <Hamburger src={menu} alt='menu' />
 
-        </Navdiv>
+            </Navdiv>
+        ) : (<span>Loading...</span>)
     )
 }
 
@@ -69,7 +98,7 @@ const Navdiv = styled.div`
     color: white;
     align-items: center;
     height: 4rem;
-    background: ${props => (props.scroll ? "#332620" : 'transparent')};
+    background: ${props => (props.scrolled ? "#332620" : 'transparent')};
     text-transform: uppercase;
     font-size: .7rem;
     position: fixed;
@@ -85,7 +114,7 @@ const Leftnav = styled.div`
     flex-direction: row;
     width: fit-content;
     gap: 2rem;
-    color: ${props => (props.scroll ? props => props.leftscroll : props.left)};
+    color: ${props => (props.scrolled ? props => props.leftscroll : props.left)};
     align-items: center;
     justify-content: center;
 `
@@ -94,7 +123,7 @@ const Rightnav = styled.div`
     flex-direction: row;
     width: fit-content;
     gap: 2rem;
-    color: ${props => (props.scroll ? props => props.rightscroll : props.right)};
+    color: ${props => (props.scrolled ? props => props.rightscroll : props.right)};
     align-items: center;
 `
 const Ul = styled.ul`
@@ -116,7 +145,7 @@ const Button = styled.li`
   background: transparent;
   border-radius: 3px;
   border: 1px solid ${props => props.color};
-  color: ${props => (props.scroll ? "black" : props => props.color)};
+  color: ${props => (props.scrolled ? "black" : props => props.color)};
   padding: 0.5rem 1.5rem;
 
   @media screen and (max-width: 850px) {
@@ -128,21 +157,14 @@ const Button = styled.li`
 const ButtonJoin = styled.li`
   background: ${props => props.joinbackground};
   border-radius: 3px;
-  border: 1px solid ${props => (props.scroll ? props => props.joinborderscroll : props.joinborder)};
-  color: ${props => (props.scroll ? props => props.joincolorscroll : props.joincolor)};
+  border: 1px solid ${props => (props.scrolled ? props => props.joinborderscroll : props.joinborder)};
+  color: ${props => (props.scrolled ? props => props.joincolorscroll : props.joincolor)};
   padding: 0.5rem 1.5rem;
 
   @media screen and (max-width: 850px) {
     display: none;
 }
 `
-// pop up
-// email
-// password
-// signin button with email and passsword
-
-// create account or signin
-// create account with google name, password, confirm, email(popup)
 
 const SelectField = styled.select`
   background: transparent;
@@ -152,12 +174,12 @@ const SelectField = styled.select`
 `
 
 const SignInButton = styled.li`
-    border-left: 1px solid ${props => (props.scroll ? props => props.siginborderscroll : props.siginborder)};
+    border-left: 1px solid ${props => (props.scrolled ? props => props.siginborderscroll : props.siginborder)};
     display: flex;
     align-items: center;
     justify-content: center;
     cursor: pointer;
-    color: ${props => (props.scroll ? props => props.signincolorscroll : props.signincolor)};
+    color: ${props => (props.scrolled ? props => props.signincolorscroll : props.signincolor)};
     padding: 0.5rem 0 0.5rem 2rem;
     text-decoration: none;
 `
@@ -169,4 +191,4 @@ const Hamburger = styled.img`
     @media screen and (max-width: 850px) {
         display: block;
     }
-`
+    `
